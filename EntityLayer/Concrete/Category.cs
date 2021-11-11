30ad9f35a8a266eba2bf4ece61343f393d507d9c
Alpay Akçer <info@alpayakcer.com>
2021-11-11 18:15:38 +0300

    Projenin Githup'a yüklenmesi

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [StringLength(70)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [StringLength(150)]
+        public string Picture { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        [StringLength(70)]
+        public string UserName { get; set; }
+        [StringLength(70)]
+        public string Surname { get; set; }
+        [StringLength(150)]
+        public string Email { get; set; }
+        [StringLength(500)]
+        public string Messge { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
